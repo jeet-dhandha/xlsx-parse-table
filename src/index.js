1b954151e9fdc0ecccba9ef7a8997b9ab340a098
jeet-dhandha <jeet.dhandha.2511@gmail.com>
2024-11-10 13:41:37 +0530

    optimise readme.md

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import * as xlsxTableParser from "./lib";
-export default xlsxTableParser;
+// Published as NPM Library
+import { extractTables } from "./xlsxParserLib";
+
+/**
+ * @typedef Table
+ * @type {Array<string>}
+ */
+// Current file - index.js
+
+/**
+ * Parse tables and return as Array of Tables
+ * @param {Object} worksheet
+ * @returns {Array<Table>}
+ */
+const parse = (worksheet) => {
+  return extractTables(worksheet);
+};
+
+export default { parse };
