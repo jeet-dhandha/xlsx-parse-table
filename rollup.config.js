86eaafe4931882e88e8e5ec09099442676cea210
jeet-dhandha <jeet.dhandha.2511@gmail.com>
2024-11-10 13:45:06 +0530

    removed extra libs

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const { terser } = require("rollup-plugin-terser");
-const json = require("@rollup/plugin-json");
-const babel = require("rollup-plugin-babel");
 
 module.exports = {
   input: "src/index.js", // Adjust the entry point as needed
@@ -13,12 +11,8 @@ module.exports = {
     },
   ],
   plugins: [
-    babel({
-      exclude: "node_modules/**",
-    }),
     resolve(), // so Rollup can find node_modules
     commonjs(), // so Rollup can convert commonjs to ES modules
-    json(), // so Rollup can import JSON files
     terser(), // to minify the bundle
   ],
 };
